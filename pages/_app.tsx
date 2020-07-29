4dfd39d8c5422eabb944d54240ec1b6cf3eeaa6a
wstar0506 <wstar0506@gmail.com>
2020-07-29 15:34:31 -0300

    Global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import {light} from '../styles/theme'
+import { light } from '../styles/theme'
 import { ThemeProvider } from 'styled-components';
+import GlobalStyles from '../styles/Global'
 
 function MyApp({ Component, pageProps }) {
     return (
         <ThemeProvider theme={light}>
+            <GlobalStyles />
             <Component {...pageProps} />
         </ThemeProvider>
     );
---
+++ styles/Global/index.ts
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle `
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: 'Inter', sans-serif;
+        -webkit-appearance: inherit;
+        -webkit-appearance:initial
+    }
+
+    ul, li {
+        list-style: none;
+    }
+    
+    button, input:focus { outline: none; }
+
+`;
