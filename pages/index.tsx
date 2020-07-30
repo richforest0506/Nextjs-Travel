5673b234806db4212abadb23fd08dd53b5dc92c8
wstar0506 <wstar0506@gmail.com>
2020-07-30 00:30:25 -0300

    Import Header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,30 @@
-import React from "react";
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Laytout from '../src/components/Layout'
+import React from 'react';
+import Head from 'next/head';
+import styled from 'styled-components';
+import Header from '../src/components/Header';
 
-export default function Home() {
+const LayoutHomePage = styled.div`
+  max-width: 1200px;
+  margin: auto;
+`;
+
+export default function Home(): JSX.Element {
   return (
     <>
       <Head>
         <title>App</title>
         <link rel="icon" href="/icon/favicon.ico" />
+        <meta charSet="utf-8" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@500&display=swap"
+          rel="stylesheet"
+        />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <Laytout/>
+      <LayoutHomePage>
+        <Header />
+      </LayoutHomePage>
       {/* <img src="/images/image-1.png" alt="Vercel" className={styles.logo}/> */}
-
     </>
-  )
+  );
 }
