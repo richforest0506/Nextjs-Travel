ac8cc5d69fbabb8222abd89c3b686e11b32073d6
wstar0506 <wstar0506@gmail.com>
2020-07-30 18:03:58 -0300

    Updated Section Testimonials

--- src/components/Testimonials/index.tsx
+++ src/components/Testimonials/index.tsx
@@ -1,17 +1,40 @@
 import React from 'react';
-import { Container, ShapeImage } from './styles';
+import {
+  Container,
+  ShapeImage,
+  ContentLeft,
+  StarIcon,
+  IconLeft,
+  IconRight,
+} from './styles';
+import { Circle, Plus } from '../Shape';
 
 const Testimonials: React.FC = () => (
   <Container>
-    <h1>Testimonials</h1>
-    <p>
-      "Quisque in lacus a urna fermentum euismod. Integer mi nibh, dapibus ac
-      scelerique eu, facilisis quis purus. Morbi blandit sit amaet turpis nex"
-    </p>
-    <strong>Edward Newgate</strong>
-    <h6>Founder Circle</h6>
+    <ContentLeft>
+      <h1>Testimonials</h1>
+      <section>
+        <StarIcon />
+        <StarIcon />
+        <StarIcon />
+        <StarIcon />
+        <StarIcon />
+      </section>
+      <p>
+        “Quisque in lacus a urna fermentum euismod. Integer mi nibh, dapibus ac
+        scelerisque eu, facilisis quis purus. Morbi blandit sit amet turpis nec”
+      </p>
+      <strong>Edward Newgate</strong>
+      <h6>Founder Circle</h6>
+    </ContentLeft>
     <ShapeImage>
-      <img src="/images/image-1.png" alt="Edward Newgate, testimonials" />
+      <Circle x="96%" y="-3px" />
+      <img src="/images/photo.svg" alt="Edward Newgate, testimonials" />
+      <footer>
+        <IconLeft />
+        <IconRight active />
+      </footer>
+      <Plus x="0px" y="95%" />
     </ShapeImage>
   </Container>
 );
--- src/components/Testimonials/styles.ts
+++ src/components/Testimonials/styles.ts
@@ -1,5 +1,110 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { MdStar } from 'react-icons/md';
+import { IoIosArrowDropright, IoIosArrowDropleft } from 'react-icons/io';
+import { shade } from 'polished';
 
-export const Container = styled.div``;
+interface IIconProps {
+  active?: boolean;
+}
 
-export const ShapeImage = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 173px;
+  align-items: center;
+`;
+
+export const ContentLeft = styled.div`
+  max-width: 50%;
+  h1 {
+    width: 100%;
+    max-width: 418px;
+    font-size: 36px;
+    font-weight: bold;
+    color: ${(props) => props.theme.colors.title};
+    margin-bottom: 145px;
+  }
+  p {
+    color: ${(props) => props.theme.colors.subtitle};
+    font-size: 24px;
+    margin: 20px 0 33px 0;
+    width: 450px;
+  }
+  strong {
+    color: ${(props) => props.theme.colors.title};
+    font-weight: bold;
+    font-size: 22px;
+  }
+  h6 {
+    color: ${(props) => props.theme.colors.title};
+    font-weight: normal;
+    font-size: 18px;
+    line-height: 48px;
+  }
+`;
+
+export const StarIcon = styled(MdStar).attrs({
+  size: '25px',
+  color: '#FFBB0C',
+})`
+  margin-right: 7px;
+`;
+
+export const ShapeImage = styled.div`
+  position: relative;
+
+  footer {
+    position: absolute;
+    background: ${(props) => props.theme.colors.background};
+    width: 132px;
+    height: 60px;
+    border-top-left-radius: 15px;
+    bottom: 0;
+    right: 0;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+`;
+
+export const IconLeft = styled(IoIosArrowDropleft).attrs({
+  size: '45px',
+})<IIconProps>`
+  margin: 0 0 0 13px;
+  margin-right: 7px;
+  fill: ${(props) => props.theme.colors.subtitle};
+  &:hover {
+    cursor: pointer;
+    fill: ${(props) => shade(0.2, props.theme.colors.subtitle)};
+  }
+  ${(props) =>
+    props.active &&
+    css`
+      fill: ${props.theme.colors.title};
+      &:hover {
+        cursor: pointer;
+        fill: ${shade(0.2, props.theme.colors.title)};
+      }
+    `};
+`;
+
+export const IconRight = styled(IoIosArrowDropright).attrs({
+  size: '45px',
+})<IIconProps>`
+  margin: 0 13px 0 0;
+  margin-right: 7px;
+  fill: ${(props) => props.theme.colors.subtitle};
+  &:hover {
+    cursor: pointer;
+    fill: ${(props) => shade(0.2, props.theme.colors.subtitle)};
+  }
+  ${(props) =>
+    props.active &&
+    css`
+      fill: ${props.theme.colors.title};
+      &:hover {
+        cursor: pointer;
+        fill: ${shade(0.2, props.theme.colors.primary)};
+      }
+    `};
+`;
