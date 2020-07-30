2a87c602021d5c3864dadad5df2fd47364abaa02
wstar0506 <wstar0506@gmail.com>
2020-07-30 12:04:04 -0300

    Created Section Guides

---
+++ src/components/SectionGuides/styles.ts
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+import image from '../../../public/images/thousand-03.svg';
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 173px;
+`;
+
+export const ContentLeft = styled.div`
+  max-width: 50%;
+  h1 {
+    width: 100%;
+    max-width: 418px;
+    font-size: 36px;
+    font-weight: bold;
+    color: ${(props) => props.theme.colors.title};
+  }
+  p {
+    color: ${(props) => props.theme.colors.subtitle};
+    font-size: 17px;
+    margin: 20px 0 33px 0;
+    max-width: 450px;
+  }
+`;
+
+export const Image = styled(image)`
+  max-width: 50%;
+  max-width: 50%;
+  height: 100%;
+`;
