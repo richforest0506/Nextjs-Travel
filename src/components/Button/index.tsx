9b44527ffc8c34eecb769524087f5eca79a08435
wstar0506 <wstar0506@gmail.com>
2020-08-01 15:13:33 -0300

    Create new type button, button Text

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
 
+import { IconBaseProps } from 'react-icons/lib';
 import { Container, Ink } from './styles';
 
 interface IPropsButton extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   outline?: boolean;
+  Icon?: React.ComponentType<IconBaseProps>;
+  buttonText?: boolean;
 }
 
-const Button: React.FC<IPropsButton> = ({ children, outline, ...props }) => (
-  <Container outline={!!outline} type="button" {...props}>
+const Button: React.FC<IPropsButton> = ({
+  children,
+  outline,
+  Icon,
+  buttonText,
+  ...props
+}) => (
+  <Container
+    outline={!!outline}
+    buttonText={buttonText}
+    type="button"
+    {...props}
+  >
+    {Icon && <Icon />}
     {children}
     <Ink />
   </Container>
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -4,24 +4,36 @@ import ReactInk from 'react-ink';
 
 interface IButtonProps {
   outline: boolean;
+  buttonText: boolean;
 }
 
 export const Container = styled.button<IButtonProps>`
   width: 132px;
   height: 44px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
   border-radius: 8px;
   cursor: pointer;
   background: ${(props) => props.theme.colors.primary};
   border: 1px solid ${(props) => props.theme.colors.primary};
-  padding: 0 16px;
+  padding: 0 18px;
   color: ${(props) => props.theme.colors.text};
   font-size: 15px;
   font-weight: 500;
   transition: background-color 0.2s;
   position: relative;
+  svg {
+    color: ${(props) => props.theme.colors.text};
+    font-size: 22px;
+  }
   &:hover {
     background: ${(props) => shade(0.2, props.theme.colors.primary)};
   }
+  & + button {
+    margin-left: 18px;
+  }
+
   ${(props) =>
     props.outline &&
     css`
@@ -32,10 +44,30 @@ export const Container = styled.button<IButtonProps>`
       &:hover {
         background: ${shade(0.2, props.theme.colors.text)};
       }
+      svg {
+        color: ${props.theme.colors.primary};
+      }
     `}
-  & + button {
-    margin-left: 18px;
-  }
+
+  ${(props) =>
+    props.buttonText &&
+    css`
+      border: none;
+      color: ${props.theme.colors.primary};
+      background: transparent;
+
+      &:hover {
+        color: ${shade(0.2, props.theme.colors.primary)};
+        background: transparent;
+        svg {
+          color: ${shade(0.2, props.theme.colors.primary)};
+        }
+      }
+
+      svg {
+        color: ${props.theme.colors.primary};
+      }
+    `}
 `;
 
 export const Ink = styled(ReactInk).attrs({
